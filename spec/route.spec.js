ccfe705dd52818ffa7e71d0637f7b8d202fa4d46
Richard Key <rich@busyrich.com>
2019-03-16 15:42:47 -0700

    basic server features, with tests

---
+++ spec/route.spec.js
@@ -0,0 +1,100 @@
+const Route = require('../lib/Route.js');
+
+describe('Route Paths', () => {
+  it('should passthrough non-parameterized paths', () => {
+    const p = '/test/test/test';
+    expect(new Route(p).path).toBe(p);
+  });
+
+  it('should change colon-params to regex params', () => {
+    const r = new Route('/test/:param/test');
+    expect(r.path).toEqual(jasmine.any(RegExp));
+    expect(r.path.toString()).toBe('/\\/test\\/(.+)\\/test/');
+    expect(r.params[0]).toBe('param');
+  });
+});
+
+describe('Route Handlers', () => {
+
+  it('should have method methods, both upper and lowercase', () => {
+    const r = new Route();
+
+    Route.METHODS.forEach(method => {
+      expect(r[method]).toEqual(jasmine.any(Function));
+      expect(r[method.toLowerCase()]).toEqual(jasmine.any(Function));
+    });
+  });
+
+  it('should add handlers based on the method used', () => {
+    const r = new Route(),
+          h = () => {}, h2 = () => {};
+
+    r.get(h);
+    expect(r.handlers['GET']).toBe(h);
+
+    r.post(h2);
+    expect(r.handlers['POST']).toBe(h2);
+  });
+
+  it('should be able to add handled during route creation', () => {
+    const g = () => {}, 
+          p = () => {},
+          r = new Route('/test', {
+            get: g,
+            post: p
+          });
+      
+    expect(r.handlers).toEqual({
+      'GET': g,
+      'POST': p
+    });
+  });
+
+  it('should be able to add a get handler during route creation', () => {
+    const g = () => {}, 
+          r = new Route('/test', g);
+      
+    expect(r.handlers).toEqual({
+      'GET': g
+    });
+  });
+
+  it('should not execute if path does not match', () => {
+    const r = new Route('/test'),
+          h = jasmine.createSpy('h');
+
+    r.get(h);
+    r.run({
+      method: 'GET',
+      path: '/nottest'
+    }, {});
+
+    expect(h).not.toHaveBeenCalled();
+  });
+
+  it('should execute handler if path does match', () => {
+    const r = new Route('/test'),
+          h = jasmine.createSpy('h');
+
+    r.get(h);
+    r.run({
+      method: 'GET',
+      path: '/test'
+    }, {});
+
+    expect(h).toHaveBeenCalled();
+  });
+
+  it('should match and parse route params when set', () => {
+    const r = new Route('/test/:myParam/test'),
+          h = req => {
+            expect(req.params.myParam).toBe(5);
+          };
+
+    r.get(h);
+    r.run({
+      method: 'GET',
+      path: '/test/5/test'
+    }, {});
+  });
+});
---
+++ spec/server.spec.js
@@ -0,0 +1,101 @@
+const request = require('request-promise-native'),
+      srv = require('../lib/Server')(),
+      urlBase = `http://localhost:${srv.port}`;
+
+srv.start();
+
+describe('Routing', () => {
+  it('should route basic requests', () => {
+    srv.route('/test', (req, res) => {
+      res.setHeader('content-type', 'text/plain');
+      res.write('Test Success');
+      res.end();
+    });
+
+    return request({
+      uri: `${urlBase}/test`,
+      resolveWithFullResponse: true  
+    }).then((res) => {
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toBe('text/plain');
+      expect(res.body).toBe('Test Success');
+    });
+  });
+
+  it('should allow returning responses from route handlers', () => {
+    srv.route('/test2', (req, res) => {
+      res.setHeader('content-type', 'text/plain');
+      return 'Test Success';
+    });
+
+    return request({
+      uri: `${urlBase}/test2`,
+      resolveWithFullResponse: true  
+    }).then((res) => {
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toBe('text/plain');
+      expect(res.body).toBe('Test Success');
+    });
+  });
+
+  it('should automatically parse JSON request bodies', () => {
+    srv.route('/test3', (req, res) => {
+      res.setHeader('content-type', 'application/json');
+      return JSON.stringify(req.body);
+    });
+
+    return request({
+      uri: `${urlBase}/test3`,
+      json: true,
+      body: {message:'Test Success'},
+      resolveWithFullResponse: true  
+    }).then((res) => {
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toBe('application/json');
+      expect(res.body.message).toBe('Test Success');
+    });
+  });
+
+  it('should automatically 404', () => {
+    return request({
+      uri: `${urlBase}/test4`,
+      resolveWithFullResponse: true  
+    })
+    .catch(err => {
+      expect(err.response.statusCode).toBe(404);
+      expect(err.response.body).toBe('404');
+    });
+  });
+});
+
+describe('Preprocessors', () => {
+  it('should add preprocessors before routes, and in order of addition', () => {
+    const p1 = (req, res, next) => next(),
+          p2 = (req, res, next) => next(),
+          p3 = (req, res, next) => next();
+
+    srv.pre(p1);
+    srv.pre(p2);
+    srv.pre(p3);
+    
+    expect(srv.chain[0]).toBe(p1);
+    expect(srv.chain[1]).toBe(p2);
+    expect(srv.chain[2]).toBe(p3);
+  });
+
+  it('should execute preprocessors before any routing', () => {
+    srv.route('/test5', (req, res) => req.test);
+    srv.pre((req, res, next) => {
+      req.test = 'Test Success';
+      next();
+    });
+
+    return request({
+      uri: `${urlBase}/test5`,
+      resolveWithFullResponse: true  
+    }).then((res) => {
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toBe('Test Success');
+    });
+  });
+});
